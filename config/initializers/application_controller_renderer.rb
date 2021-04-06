e2ec2a972e046e8eefba24c38d74b056f85c606d
EC2 Default User <ec2-user@ip-172-31-91-91.ec2.internal>
2021-04-06 06:46:04 +0000

    [finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '51cce9ea4a89d6c1bbbe8c57d070b5683b674400f87ee7956a9e007622ec935a9527a3af1e41e53b24c1553b879763ba8ba4ea6735a85541ce52c903952ac123'
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_023212) do
+ActiveRecord::Schema.define(version: 2021_04_06_054229) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
